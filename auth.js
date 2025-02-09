683fe2c1973226bf5b237cc7cef14f58b2c84e20
G-Nash <20b91a5725@srkrec.ac.in>
2025-02-09 13:28:01 +0530

    authentication done

--- auth.js
+++ auth.js
@@ -1,3 +1,44 @@
-import{
-    getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword
-} from "./firebase.js"
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "./firebase.js";
+
+let loginform = document.getElementById("loginform");
+let signupform = document.getElementById("signupform");
+
+if(signupform){
+    signupform.addEventListener("submit",(e) => {
+        e.preventDefault();
+      
+        let auth = getAuth();
+        let email = e.target[0].value;
+        let password = e.target[1].value;
+      
+        createUserWithEmailAndPassword(auth, email, password)
+          .then((x) => {
+            console.log(x);
+            alert("User registered successfully");
+            location.assign("./login.html");
+          })
+          .catch(() => {});
+      });
+}
+
+if(loginform){
+    loginform.addEventListener("submit",(e) => {
+        e.preventDefault();
+      
+        let auth = getAuth();
+        let email = e.target[0].value;
+        let password = e.target[1].value;
+      
+        signInWithEmailAndPassword(auth, email, password)
+          .then((x) => {
+              console.log(x.user.accessToken);
+              alert("logged in successfully");
+              location.replace("./home.html")
+          })
+          .catch(() => {});
+      });
+}
