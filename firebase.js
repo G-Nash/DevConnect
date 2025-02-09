d9930f97d54035dfa03f6f0ee6048a1ee8ae4ec9
G-Nash <20b91a5725@srkrec.ac.in>
2025-02-09 12:09:16 +0530

    firebase

--- firebase.js
+++ firebase.js
@@ -1,11 +1,10 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth , createUserWithEmailAndPassword , signInWithEmailAndPassword  } from 'https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyC8vORJHCG6b7cZlz03WFQvs4LtrKj9C9U",
   authDomain: "devconnect-1b3fa.firebaseapp.com",
@@ -16,6 +15,7 @@ const firebaseConfig = {
   measurementId: "G-YZT8BXWTTY",
 };
 
-// Initialize Firebase
+
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
