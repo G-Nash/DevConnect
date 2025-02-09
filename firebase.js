527f92d8250d1b0dd5f1b7f5660e5f65f6c61151
G-Nash <20b91a5725@srkrec.ac.in>
2025-02-09 12:00:53 +0530

    branch new authentication

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC8vORJHCG6b7cZlz03WFQvs4LtrKj9C9U",
+  authDomain: "devconnect-1b3fa.firebaseapp.com",
+  projectId: "devconnect-1b3fa",
+  storageBucket: "devconnect-1b3fa.firebasestorage.app",
+  messagingSenderId: "571904422946",
+  appId: "1:571904422946:web:dfd12a8f0a6f5c6b6f467b",
+  measurementId: "G-YZT8BXWTTY",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
