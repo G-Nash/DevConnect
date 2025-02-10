9476dc6e8e5aa89c3160dc0f8e4a7e0488f05238
G-Nash <20b91a5725@srkrec.ac.in>
2025-02-10 11:30:00 +0530

    darkLight

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ function darkLight(){
         document.documentElement.style.setProperty("--textbasic","#000000")
         document.documentElement.style.setProperty("--marginColor","#00000080")
         document.documentElement.style.setProperty("--accentColor","#a9a9a9")
+        document.documentElement.style.setProperty("--textaccentColor","#00000080")
     }
     else{
         document.documentElement.style.setProperty("--backgroundColor","#0E1217")
         document.documentElement.style.setProperty("--textbasic","#ffffff")
         document.documentElement.style.setProperty("--marginColor","#d3d3d380")
         document.documentElement.style.setProperty("--accentColor","#1C1F26")
+        document.documentElement.style.setProperty("--textaccentColor","#ffffff80")
     }
 }
