e4b74b0f243d6b28fb13f2afa063e5da062e81bf
G-Nash <20b91a5725@srkrec.ac.in>
2025-02-11 10:00:09 +0530

    darklight

--- homefeed.js
+++ homefeed.js
@@ -1,42 +1,41 @@
-let sideBarButtons=document.getElementsByClassName("sideBarButtons")
-
-function darkLight(){
-    if (getComputedStyle(document.documentElement).getPropertyValue("--backgroundColor")!="#e1e1e1"){
-        document.documentElement.style.setProperty("--backgroundColor","#e1e1e1")
-        document.documentElement.style.setProperty("--textbasic","#000000")
-        document.documentElement.style.setProperty("--marginColor","#00000080")
-        document.documentElement.style.setProperty("--accentColor","#a9a9a9")
-        document.documentElement.style.setProperty("--textaccentColor","#00000080")
-        for (let btn of sideBarButtons) {
-          btn.style.color = "black";
-        }
-    }
-    else{
-        document.documentElement.style.setProperty("--backgroundColor","#0E1217")
-        document.documentElement.style.setProperty("--textbasic","#ffffff")
-        document.documentElement.style.setProperty("--marginColor","#d3d3d380")
-        document.documentElement.style.setProperty("--accentColor","#1C1F26")
-        document.documentElement.style.setProperty("--textaccentColor","#ffffff80")
-        for (let btn of sideBarButtons) {
-          btn.style.color = "white";
-      }
-    }
+function darkLight() {
+  if (
+    getComputedStyle(document.documentElement).getPropertyValue(
+      "--backgroundColor"
+    ) != "#e1e1e1"
+  ) {
+    document.documentElement.style.setProperty("--backgroundColor", "#e1e1e1");
+    document.documentElement.style.setProperty("--textbasic", "#000000");
+    document.documentElement.style.setProperty("--marginColor", "#00000080");
+    document.documentElement.style.setProperty("--accentColor", "#a9a9a9");
+    document.documentElement.style.setProperty(
+      "--textaccentColor",
+      "#00000080"
+    );
+  } else {
+    document.documentElement.style.setProperty("--backgroundColor", "#0E1217");
+    document.documentElement.style.setProperty("--textbasic", "#ffffff");
+    document.documentElement.style.setProperty("--marginColor", "#d3d3d380");
+    document.documentElement.style.setProperty("--accentColor", "#1C1F26");
+    document.documentElement.style.setProperty(
+      "--textaccentColor",
+      "#ffffff80"
+    );
+  }
 }
 
+let logout = document.getElementById("navBarLogout");
 
-let logout=document.getElementById("navBarLogout")
-
-if(logout){
-    logout.addEventListener("click", ()=>{
-        location.replace("./index.html")
-    })
+if (logout) {
+  logout.addEventListener("click", () => {
+    location.replace("./index.html");
+  });
 }
 
+let sideLogout = document.getElementById("sideBarLogOut");
 
-let sideLogout=document.getElementById("sideBarLogOut")
-
-if(sideLogout){
-    sideLogout.addEventListener("click",()=>{
-        location.replace("./index.html")
-    })
+if (sideLogout) {
+  sideLogout.addEventListener("click", () => {
+    location.replace("./index.html");
+  });
 }
