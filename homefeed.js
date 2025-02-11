bd6517d74b5e67501876544962f1d7eea5747b25
G-Nash <20b91a5725@srkrec.ac.in>
2025-02-11 09:55:43 +0530

    button color

--- homefeed.js
+++ homefeed.js
@@ -1,3 +1,5 @@
+let sideBarButtons=document.getElementsByClassName("sideBarButtons")
+
 function darkLight(){
     if (getComputedStyle(document.documentElement).getPropertyValue("--backgroundColor")!="#e1e1e1"){
         document.documentElement.style.setProperty("--backgroundColor","#e1e1e1")
@@ -5,6 +7,9 @@ function darkLight(){
         document.documentElement.style.setProperty("--marginColor","#00000080")
         document.documentElement.style.setProperty("--accentColor","#a9a9a9")
         document.documentElement.style.setProperty("--textaccentColor","#00000080")
+        for (let btn of sideBarButtons) {
+          btn.style.color = "black";
+        }
     }
     else{
         document.documentElement.style.setProperty("--backgroundColor","#0E1217")
@@ -12,6 +17,9 @@ function darkLight(){
         document.documentElement.style.setProperty("--marginColor","#d3d3d380")
         document.documentElement.style.setProperty("--accentColor","#1C1F26")
         document.documentElement.style.setProperty("--textaccentColor","#ffffff80")
+        for (let btn of sideBarButtons) {
+          btn.style.color = "white";
+      }
     }
 }
 
